a89a7ed512fade2d30694203faa75b2c209bafa7
awestover <alek.westover@gmail.com>
2018-04-15 16:35:52 -0400

    pic added

--- pyopengl/cubeGame.py
+++ pyopengl/cubeGame.py
@@ -13,7 +13,26 @@ from basics import *
 import numpy as np
 from numpy import pi, sin, cos
 
+import random
+from random import random as rand
 from pdb import set_trace as tr
+
+class Ground():
+    def __init__(self):
+        self.verts = (
+            (-10, -3, 20),
+            (10, -3, 20),
+            (-10, -3, -300),
+            (10, -3, -300)
+            )
+        self.color = (rand(),rand(),rand())
+
+    def draw(self):
+        glBegin(GL_QUADS)
+        for vertex in self.verts:
+            glColor3fv(self.color)
+            glVertex3fv(vertex)
+        glEnd()
 
 class Cube():
     def __init__(self, pos=(0,0,0)):
@@ -23,22 +42,27 @@ class Cube():
 
     def draw(self):
         glBegin(GL_LINES)
-        try:
-            for edge in self.edges:
-               for vertex in edge:
-                    glVertex3fv(addt(self.base_verticies[vertex], self.pos))
-        except:
-            tr()
+        for edge in self.edges:
+           for vertex in edge:
+                glVertex3fv(addt(self.base_verticies[vertex], self.pos))
         glEnd()
+
+    def scramble_pos(self,k):
+        self.pos = ((rand()-0.5)*k,(rand()-0.5)*k,(rand()-0.5)*k)
 
 class Asteroid():
     def __init__(self, pos=(0,0,0), size=1):
         self.pos = pos
 
+        if rand()<0.5:
+            self.skeleton = True
+        else:
+            self.skeleton = False
+
         ths = np.linspace(0, 2*pi, 8)
         phs = np.linspace(0,   pi, 8)
         rho_avg = size
-        rho_dev = 0.4*size
+        rho_dev = 0.004*size
 
         self.base_verticies = []
         for thi in range(0, len(ths)):
@@ -58,17 +82,22 @@ class Asteroid():
         return (r, g, b)
 
     def draw(self):
-        glBegin(GL_TRIANGLES)
+        if not self.skeleton:
+            glBegin(GL_TRIANGLES)
+        else:
+            glBegin(GL_LINES)
 
         for thi in range(0, len(self.base_verticies)):
             for phi in range(0, len(self.base_verticies[0])):
                 glColor(self.color(phi, thi))
-                
+                # glColor((1,1,1))
+
                 glVertex3fv(addt(self.base_verticies[thi][phi], self.pos))
                 glVertex3fv(addt(self.base_verticies[(thi+1)%len(self.base_verticies)][phi], self.pos))
                 glVertex3fv(addt(self.base_verticies[thi][(phi+1)%len(self.base_verticies[0])], self.pos))
 
-                glColor((0,0,0))
+                glColor(self.color(phi, thi))
+                # glColor((0,0,0))
                 glVertex3fv(addt(self.base_verticies[(thi+1)%len(self.base_verticies)][phi], self.pos))
                 glVertex3fv(addt(self.base_verticies[thi][(phi+1)%len(self.base_verticies[0])], self.pos))
                 glVertex3fv(addt(self.base_verticies[(thi+1)%len(self.base_verticies)][(phi+1)%len(self.base_verticies[0])], self.pos))
@@ -76,50 +105,91 @@ class Asteroid():
         glEnd()
 
 
+    def scramble_pos(self,k):
+        self.pos = ((rand()-0.5)*k,(rand()-0.5)*k,(rand()-0.5)*k)
+
 
 def main():
     pygame.init()
     display = (1200,800)
     pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
 
-    gluPerspective(45, (display[0]/display[1]), 0.1, 50.0)
+    gluPerspective(45, (display[0]/display[1]), 1.0, 30.0)
+    glClearColor(rand(),rand(),rand(),rand())
+    glTranslatef(0,0,-20.0)
 
-    glTranslatef(0.0,0.0, -5)
+    ground = Ground()
 
-    cubes = [Asteroid((2,0,0), size=0.1), Asteroid((0,0,0),size=0.5)]
+    cubes = [Cube((0,0,0))]
+    for i in range(0, random.randint(1,10)):
+        cubes.append(Asteroid(((rand()-0.5)*10,(rand()-0.5)*10,(rand()-0.5)*10), size=rand()))
 
-    # background
-    glClearColor(1,1,0,0)
+    done = 10
 
-    while True:
+    while done > 0:
         keys = pygame.key.get_pressed()  #checking pressed keys
+        
         if keys[pygame.K_a]:
-            glRotatef(1, 0, 1, 0)
+            glRotatef(5, 0, 1, 0)
         if keys[pygame.K_d]:
-            glRotatef(1, 0, -1, 0) 
+            glRotatef(5, 0, -1, 0)
+
         if keys[pygame.K_w]:
-            glRotatef(1, 0, 0, 1)
+            glRotatef(5, 0, 0, 1)
         if keys[pygame.K_s]:
-            glRotatef(1, 0, 0, -1) 
-        if keys[pygame.K_DOWN]:
-            glRotatef(1, 1, 0, 0)
+            glRotatef(5, 0, 0, -1)
+
+        # if keys[pygame.K_g]:
+        #     glRotatef(5, 1, 0, 0)
+        # if keys[pygame.K_h]:
+        #     glRotatef(5, -1, 0, 0)
+
+        if keys[pygame.K_LEFT]:
+            glTranslatef(0.1,0,0)
+        if keys[pygame.K_RIGHT]:
+            glTranslatef(-0.1,0,0)
+
         if keys[pygame.K_UP]:
-            glRotatef(1, -1, 0, 0)
+            glTranslatef(0,-0.1,0)
+        if keys[pygame.K_DOWN]:
+            glTranslatef(0,0.1,0)
+
+        # if keys[pygame.K_c]:
+        #     glTranslatef(0,0,0.1)
+        # if keys[pygame.K_z]:
+        #     glTranslatef(0,0,-0.1)
+
 
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
-                if event.key==pygame.K_d:
-                    print("d")
-                elif event.key == pygame.K_a:
-                    print("a")
+                if event.key==pygame.K_q:
+                    pygame.quit()
+                    quit()
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
 
         glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
         
+        ground.draw()
         for cube in cubes:
             cube.draw()
+
+        glTranslatef(0,0,0.3)
+
+
+        x = glGetDoublev(GL_MODELVIEW_MATRIX)
+
+        camera_x = x[3][0]
+        camera_y = x[3][1]
+        camera_z = x[3][2]
+
+        if camera_z < 2:
+            done-=1
+            glClearColor(rand(),rand(),rand(),rand())
+            glTranslatef(0,0,-20.0)
+            for cube in cubes:
+                cube.scramble_pos(10)
 
         pygame.display.flip()
         pygame.time.wait(10)
@@ -127,3 +197,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
