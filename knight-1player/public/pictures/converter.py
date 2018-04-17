e28bcebed4d9c3eae4a9c95505586ad37a12f626
awestover <alek.westover@gmail.com>
2018-04-17 18:35:06 -0400

    added pics

---
+++ knight-1player/public/pictures/converter.py
@@ -0,0 +1,30 @@
+# converts white to transparent backgruond
+from PIL import Image
+import os
+
+from os.path import join
+
+def imgTransparent(imgPath):
+
+	img = Image.open(imgPath)
+	img = img.convert("RGBA")
+	datas = img.getdata()
+
+	newData = []
+	for item in datas:
+		if item[0] == 255 and item[1] == 255 and item[2] == 255:
+			newData.append((255, 255, 255, 0))
+		else:
+			newData.append(item)
+
+	img.putdata(newData)
+
+	img.save(join("batch","{}").format(imgPath), "PNG")
+
+#os.mkdir("batch")
+for file in os.listdir():
+	if ".png" in file:
+		imgTransparent(file)
+
+
+
