aff4f6c3c1cc388a8f865497f8dc2b75f59092ab
awestover <alek.westover@gmail.com>
2018-04-17 18:55:56 -0400

    moves

--- knight-1player/public/Animal.js
+++ knight-1player/public/Animal.js
@@ -1,12 +1,22 @@
 function Animal(animal_traits)
 {
   this.pos = animal_traits["pos"];
+
+  var thInit = Math.random()*Math.PI*2;
+  var xv = Math.cos(thInit);
+  var yv = Math.sin(thInit);
+  this.vel = [xv, yv];
   // this.name = animal_traits["name"];
-  this.name="shark";
+  this.name="dog";
   this.image = false;
+  this.level = 1;
   if (this.name == "shark")
   {
-    this.image = loadImage("pictures/shark.png");
+    this.image = loadImage("pictures/batch/shark.png");
+  }
+  else if (this.name == "dog")
+  {
+    this.image = loadImage("pictures/batch/dog1.png");
   }
 }
 
@@ -31,10 +41,16 @@ Animal.prototype.getPos = function()
 
 Animal.prototype.move = function()
 {
-  var th = (Math.random()-0.5)/16;
-  var nx = this.pos[0]*Math.cos(th) - this.pos[1]*Math.sin(th);
-  var ny = this.pos[0]*Math.sin(th) + this.pos[1]*Math.cos(th);
-  this.pos = [nx, ny];
+  var th = (Math.random()-0.5);
+  if (Math.random() < 0.5) {
+    th = 0;
+  }
+  var nx = this.vel[0]*Math.cos(th) - this.vel[1]*Math.sin(th);
+  var ny = this.vel[0]*Math.sin(th) + this.vel[1]*Math.cos(th);
+
+  this.vel = [nx, ny];
+  this.pos = [this.pos[0] + nx, this.pos[1] + ny];
+
 }
 
 
--- knight-1player/public/sketch.js
+++ knight-1player/public/sketch.js
@@ -1,11 +1,8 @@
 // main user interaction
 
-// let socket;
 let user;
 let screen_dims;
 let canvas;
-
-// let otherUsers = {};
 
 function setup()
 {
@@ -13,17 +10,10 @@ function setup()
   canvas = createCanvas(screen_dims[0]*0.9, screen_dims[1]*0.9);
   frameRate(10);
 
-  // socket = io.connect();
-
   // let name = prompt("Name");
   let name = "alek";
-  // socket.emit("named", {"name":name});
-  // let world = prompt("World");
   let world = "the land";
   user = new User(name, world);
-
-  // socket.on("updatePlayer", handleUpdatePlayer);
-  // socket.on("deletePlayer", handleDeletePlayer);
 
 }
 
@@ -37,68 +27,7 @@ function draw()
   {
     user.animals[an].show();
     user.animals[an].move();
-    // console.log(user.animals[an]);
   }
 
-  // let data = {
-    // "user": user
-  // };
-  // socket.emit("updatePlayer", data);
-
-
-  // var msg = "";
-  // for (var otherUser in otherUsers)
-  // {
-  //   // console.log(otherUsers[otherUser]);
-  //   // msg += otherUser["animals"];
-  //   for (var cAnimal in otherUsers[otherUser]["animals"])
-  //   {
-  //     otherUsers[otherUser]["animals"][cAnimal].show();
-  //     // console.log(otherUsers[otherUser]["animals"][cAnimal].getPos());
-  //   }
-  // }
-  // console.log(msg);
 
 }
-//
-// function handleUpdatePlayer(data)
-// {
-//   // console.log("update handle");
-//   // console.log(data["user"]["name"]);
-//
-//   // console.log();
-//
-//   var cAnimals = [];
-//   for (var i = 0; i < data["user"]["animals"].length; i++)
-//   {
-//     cAnimals.push(new Animal(data["user"]["animals"][0]));
-//     // cAnimals.push(new Animal({"pos": data["user"]["animals"][i]}));
-//   }
-//
-//   var new_data = {
-//     "animals": cAnimals,
-//     "name": data["user"]["name"]
-//   }
-//   otherUsers[data["user"]["name"]] = new_data;
-//
-//   // don't really need User objects...
-//   // does it exist?
-//   // if (otherUsers[data["user"]["name"]])
-//   // {
-//   //   otherUsers[new_data["name"]].setAnimalsPos(new_data["animals"])
-//   // }
-//   // else {
-//   //   otherUsers[new_data["name"]] = new User();
-//   // }
-//
-//
-// }
-//
-// function handleDeletePlayer(data)
-// {
-//   console.log("deleting" + data["name"]);
-//   delete otherUsers[data["name"]];
-// }
-
-
-//
