3bf3ee0803cb6882ef90541186b7145d20a6d81e
awestover <alek.westover@gmail.com>
2018-04-21 16:38:42 -0400

    sketchy fix to movement problem

--- knight-1player/public/sketch.js
+++ knight-1player/public/sketch.js
@@ -5,6 +5,7 @@ let screen_dims;
 let canvas;
 
 let last_down = [0, 0];
+let isDown = false;
 
 const animal_names = ["dog", "shark"];
 const animal_size = [66, 50];
@@ -23,7 +24,6 @@ function setup()
   $('#name').text("Name: " + name);
   $('#world').text("World: " + world);
 
-
 }
 
 function draw()
@@ -36,34 +36,48 @@ function draw()
   pop();
 
   fill(0, 0, 0);
-  ellipse(screen_dims[0]/2, screen_dims[1]/2, 10, 10);
+  rect(screen_dims[0]/2-5, screen_dims[1]/2-0.25, 10, 0.5);
+  rect(screen_dims[0]/2-0.25, screen_dims[1]/2-5, 0.5, 10);
 
   user.update();
 
+  if (isDown)
+  {
+    let current_pos = [mouseX, mouseY];
+    if (onCanvas(current_pos))
+    {
+      user.updateView(last_down, current_pos);
+      last_down = [mouseX, mouseY];
+    }
+    else {
+      isDown = false;
+    }
+  }
+
 }
 
-// do it continously now
-function touchMoved() {
-  let current_pos = [mouseX, mouseY];
-  user.updateView(last_down, current_pos);
-  last_down = [mouseX, mouseY];
-  return false;
-}
+// function touchMoved() {
+//   return false
+// }
+//
 
+
+// these are a little bit sketchy
 function touchStarted()
 {
-  last_down = [mouseX,mouseY];
-  return false;
+  last_down = [mouseX, mouseY];
+  isDown = true;
 }
 
-// function touchEnded()
-// {
-//   let current_pos = [mouseX, mouseY];
-//   user.updateView(last_down, current_pos);
-//   // console.log(current_pos);
-//   return false;
-// }
+function touchEnded()
+{
+  isDown = false;
+}
 
+function onCanvas(pos)
+{
+  return (pos[0] > 0 && pos[0] < screen_dims[0] && pos[1] > 0 && pos[1] < screen_dims[1]);
+}
 
 // pick random element from list
 function pickRandom(list)
