466612a0547b92dc6982dc13ebd8e713ae24ea25
awestover <alek.westover@gmail.com>
2018-04-21 21:50:56 -0400

    addition

--- public/Animal.js
+++ public/Animal.js
@@ -1,14 +1,97 @@
+// animal class
 function Animal(animal_traits)
 {
   this.pos = animal_traits["pos"];
+  this.name = animal_traits["name"];
+  this.level = 1;
+
+  var thInit = Math.random()*Math.PI*2;
+  var xv = Math.cos(thInit);
+  var yv = Math.sin(thInit);
+  this.vel = [xv, yv];
+  this.image=this.getImg();
+}
+
+Animal.prototype.getImg = function()
+{
+  let path = "pictures/";
+  path += this.name;
+  path += this.level;
+  path += ".png";
+  try {
+    return loadImage(path);
+  }
+  catch(err)
+  {
+    console.log(err);
+    return false;
+  }
 }
 
 Animal.prototype.show = function()
 {
-  ellipse(this.pos[0], this.pos[1], 10, 10);
+  let result = false;
+
+  let offspringPr = 0.01;
+  if (offspringPr > Math.random())
+  {
+    result = this.createOffspring();
+  }
+
+  try
+  {
+    image(this.image, this.pos[0], this.pos[1], this.image.width, this.image.height);
+  }
+  catch(err)
+  {
+    ellipse(this.pos[0], this.pos[1], 10, 10);
+  }
+
+  return result;
+
 }
 
 Animal.prototype.getPos = function()
 {
   return this.pos;
+}
+
+Animal.prototype.getDims = function()
+{
+  // should be same as animal_size
+  return [this.image.width, this.image.height];
+}
+
+Animal.prototype.addPos = function(apos)
+{
+  this.pos[0] += apos[0];
+  this.pos[1] += apos[1];
+}
+
+Animal.prototype.subPos = function(apos)
+{
+  this.pos[0] -= apos[0];
+  this.pos[1] -= apos[1];
+}
+
+// later you should have animals have traits that kind of mutate and get passed down.
+Animal.prototype.createOffspring = function()
+{
+  let newAnimal = new Animal({"pos":this.pos, "name":this.name});
+  newAnimal.addPos([3*random(), 3*random()]);
+  return newAnimal;
+}
+
+Animal.prototype.move = function()
+{
+  var th = (Math.random()-0.5);
+  if (Math.random() < 0.5) {
+    th = 0;
+  }
+  var nx = this.vel[0]*Math.cos(th) - this.vel[1]*Math.sin(th);
+  var ny = this.vel[0]*Math.sin(th) + this.vel[1]*Math.cos(th);
+
+  this.vel = [nx, ny];
+  this.pos = [this.pos[0] + nx, this.pos[1] + ny];
+
 }
--- public/User.js
+++ public/User.js
@@ -1,22 +1,109 @@
-function User(name, world)
+//user class
+function User(name, world, animal_type)
 {
   this.name = name;
   this.world = world;
-  this.animals = [new Animal({"pos":[Math.random()*100, Math.random()*100]})];
+  this.animal_type = animal_type;
+
+  this.pos = [0,0]; // where are you looking
+
+  this.knights = 0;
+  // cost and level are probably going to be linked
+  this.cost = 10;
+
+  this.animals = [];
+  this.addAnimal();
+
 }
 
 User.prototype.show = function()
 {
-  return (name);
+  let results = [];
+  for (var an in user.animals)
+  {
+    let currentResult = this.animals[an].show();
+    if (currentResult != false)
+    {
+      results.push(currentResult);
+    }
+  }
+  return results;
+}
+
+User.prototype.update = function()
+{
+  for (var an in user.animals)
+  {
+    this.animals[an].move();
+  }
+  this.addFrameKnights();
+}
+
+User.prototype.addFrameKnights = function()
+{
+  // later this can be variable or something
+  if(Math.random() < 0.5)
+  {
+    this.knights += 1;
+    this.setKnightsText();
+  }
+}
+
+User.prototype.addAnimal = function()
+{
+  var randAnimal = new Animal({
+    "pos":[screen_dims[0]/2, screen_dims[1]/2],
+    "name": this.animal_type
+  });
+  this.animals.push(randAnimal);
+  this.animals[this.animals.length-1].subPos([animal_size[0]/2,animal_size[1]/2]);
+  this.setAnimalsText();
+}
+
+User.prototype.addOffspringAnimal = function(animal)
+{
+  this.animals.push(animal);
+  this.setAnimalsText();
+}
+
+User.prototype.buyAnimal = function()
+{
+  if (this.knights >= this.cost)
+  {
+    this.knights -= this.cost;
+    this.addAnimal();
+  }
 }
 
 
-User.prototype.getWorld = function()
+User.prototype.updateView = function(last_pos, current_pos)
 {
-  return this.world;
+  this.shiftPos([current_pos[0]-last_pos[0], current_pos[1] - last_pos[1]]);
 }
 
-User.prototype.getName = function()
+User.prototype.shiftPos = function(pos)
 {
-  return this.name;
+  this.pos = [this.pos[0] + pos[0], this.pos[1] + pos[1]];
+  console.log(this.pos);
+}
+
+// later only show things that are in our view
+User.prototype.inView = function(pos)
+{
+  let sPos = this.shiftPos(pos);
+  if (sPos[0]>0 && sPos[0] < screen_dims[0] && sPos[1]>0 && sPos[1]<screen_dims[1])
+  {
+    return true;
+  }
+  return false;
+}
+
+User.prototype.setKnightsText = function()
+{
+    $('#knights').text("Knights: " + this.knights);
+}
+
+User.prototype.setAnimalsText = function()
+{
+    $('#animals').text("Animals: " + this.animals.length);
 }
