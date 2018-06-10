97acca3d9978f27f40a5cb0c0c3059aaf5c00c80
awestover <alek.westover@gmail.com>
2018-06-10 19:26:43 -0400

    edits

--- index.js
+++ index.js
@@ -20,14 +20,14 @@ function queryDb(qu, callback, params)
   });
   client.connect()
     .then(() => reallyQueryDb(client, qu, callback, params))
-    .catch(e => console.error('connection error', err.stack))
+    .catch(e => console.error('connection error', e))
 }
 
-function reallyQueryDb(client, qu, callback, params)
+async function reallyQueryDb(client, qu, callback, params)
 {
   console.log("Querying " + qu);
   var results = [];
-  client.query(qu, (err, res) => {
+  await client.query(qu, (err, res) => {
     if (err) throw err;
     for (let row of res.rows) {
       var cRow = JSON.stringify(row);
