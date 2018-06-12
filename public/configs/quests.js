e7d16048f60277a8341644255a03294e5d45474f
awestover <alek.westover@gmail.com>
2018-06-11 21:02:33 -0400

    quests

--- public/configs/quests.js
+++ public/configs/quests.js
@@ -54,9 +54,7 @@ function questComplete()
     return true;
   }
   let res = getQuest(userDb["quest"]);
-  console.log(userDb[quests[res[0].toLowerCase()]);
-  console.log(res);
-  if (userDb[quests[res[0].toLowerCase()]] >= res[1])
+  if (userDb[res[0].toLowerCase()] >= res[1])
   {
     return true;
   }
