3d3fe60a636ea31619d56d9b51dc2ddd872f90c4
awestover <alek.westover@gmail.com>
2018-06-13 20:26:11 -0400

    edits

--- index.js
+++ index.js
@@ -133,7 +133,7 @@ function newConnection(socket) {
   function handleUpdateAchievments(data)
   {
     let unm = data["unm"];
-    queryDb("UPDATE Users SET $1=($2) WHERE name=$3;", [data["col"], data["newVal"], unm]);
+    queryDb("UPDATE Users SET ($1)=$2 WHERE name=$3;", [data["col"], data["newVal"], unm]);
   }
 
   function handleSelectDb(data)
