2cdb1445d78920a7956d85d1b60c8e74bca65987
awestover <alek.westover@gmail.com>
2018-06-26 22:54:34 -0400

    fAGe

--- public/code/Personal.js
+++ public/code/Personal.js
@@ -24,7 +24,7 @@ class Personal extends Animal
 
   healthAffects()
   {
-    let fAge = 0.5;
+    let fAge = 0.4;
     let ptemp = (fAge - Math.abs(this.age - fAge));
 
     this.health += ptemp * 0.025;
